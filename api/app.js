28e90f98c1a6e8e2d9981d5a1d5f272f2dc2d0e8
luanasj <luanasj@outlook.com.br>
2025-04-24 11:27:16 -0300

    feat: custom get request function to call scrapping

--- api/app.js
+++ api/app.js
@@ -1,16 +1,21 @@
 import express from 'express'
+import { dataScraping } from '../services/catalogSearch'
+
 const app = express()
 const port = 3000
 
-app.get('/api/scrape', (req, res) => {
+app.get('/api/scrape', async (req, res) => {
     const {keyword} = req.query
 
      if(keyword){
-        //calls scraping function
-     }   
-  res.send('Hello World! ' + keyword)
+        const productsColection = await dataScraping(keyword)
+
+        productsColection.length ? res.status(200).json(productsColection) : res.status(204).json({ message: 'No products found for the given keyword.' });
+     } else{
+        res.status(400).json({message: 'Bad Request: query param "keyword" is missing.'})
+     }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })

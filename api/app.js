c6b673ba75d0826f1c5008e3d172b7caca93bb34
luanasj <luanasj@outlook.com.br>
2025-04-25 12:27:00 -0300

    docs: create comments about api logic and methods

--- api/app.js
+++ api/app.js
@@ -1,19 +1,27 @@
+//Import dependences to server
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+
+//Import web scrapping function
 import { dataScraping } from '../services/catalogSearch'
 
+//Create express/server instance
 const app = express()
 const port = 3000
 
+//Integrates cors lib to avoid cors access restrictions on web requisitions
 app.use(cors());
 
+//shares static files with express so the server will be able to serve them
 app.use(express.static(path.join(__dirname, '../dist')));
 
+//Defines the web page as content to root route
 app.get('/', (req,res)=>{
    res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
+//Creates route to request scrapping based on a query
 app.get('/api/scrape', async (req, res) => {
     const {keyword} = req.query
 
@@ -26,6 +34,8 @@ app.get('/api/scrape', async (req, res) => {
      }
 })
 
+
+//starts server
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })

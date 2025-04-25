018d3dae11843072a1888e98a4d429a220067bda
luanasj <luanasj@outlook.com.br>
2025-04-25 11:35:50 -0300

    feat: calls error handling on request to amazon website

--- services/catalogSearch.js
+++ services/catalogSearch.js
@@ -7,9 +7,9 @@ const amazonSearch = async (keyword) =>{
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.9'
         }
-    })
+    }).catch((e)=>{console.log(e)})
 
-    return response.status == 200 ? response.data : undefined
+    return response?.status == 200 ? response.data : undefined
 
 }
 
